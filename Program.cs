7e27f9d421e25f4ebfc4d239d05b602210f5ce4c
Dan Vyhlidal <dan.vyhlidal@seznam.cz>
2026-05-29 10:51:32 +0200

    feat: adding scope for the app runtime, to make it available for garbage collection sooner and separate runtime logic from the exit logic

--- Program.cs
+++ Program.cs
@@ -2,13 +2,15 @@ using TriangleClassifier.App;
 using TriangleClassifier.App.Input;
 using TriangleClassifier.App.Models;
 
-IInputProvider<Triangle> triangleInputProvider = args.Length > 0 
-    ? new ArgumentInputProvider(args)
-    : new InteractiveInputProvider();
+{
+    IInputProvider<Triangle> triangleInputProvider = args.Length > 0 
+        ? new ArgumentInputProvider(args)
+        : new InteractiveInputProvider();
 
-var app = new TriangleClassificationApp(triangleInputProvider);
-app.Prepare();
-app.Run();
+    var app = new TriangleClassificationApp(triangleInputProvider);
+    app.Prepare();
+    app.Run();
+}
 
 // Awaiting enter press to not close right away the console window if this program runs through .exe file
 Console.WriteLine("Press enter to exit");
